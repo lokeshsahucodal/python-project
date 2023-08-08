1f4feb4528df50e72abe6dd4cc3fded20e415e36
Lokesh Sahu <lsahu@codal.com>
2023-08-08 16:04:52 +0530

    add test cass

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.template.defaultfilters import slugify
+from .models import Product
 
-# Create your tests here.
+
+class ModelsTestCase(TestCase):
+    def test_create_product(self):
+        name = "Test Product"
+        post, created = Product.objects.get_or_create(name=name, slug=slugify(name), image='https://st4.depositphotos.com/14431644/37827/i/450/depositphotos_378271014-stock-photo-word-writing-text-product-test.jpg')
+        self.assertEqual(post.slug, slugify(name))
